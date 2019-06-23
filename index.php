7204ec5177b0f2f93065a61b795341e04d68826c
rkelman <robb.kelman@gmail.com>
2019-06-22 19:30:15 -0700

    [0.0.1] created favicon and initial index

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="content-script-type" content="text/javascript" />
+		<meta http-equiv="content-style-type" content="text/css" />
+		<meta http-equiv="content-language" content="en-US" />
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		
+		<meta name="author" content="Robb Kelman" />	
+		<meta name="description" content="I'm Robb Kelman, an Innovative Technology Leader in Software Development and Massive Enterprise Management." />
+		<meta name="keywords" content="Robert, Kelman, Innovation, Technology, Leader, Resume, Contact, Algorithms, Cloud, DevOps, Security" />
+		<meta name="robots" content="index, follow" />
+		<meta name="revisit-after" content="14 days" />
+
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="/favicon.ico" type="image/x-icon">
+
+		<title>Robert Kelman - Innovative. Technology. Leader</title>
+  </head>
+  <body>
+      <p>Robert Kelman</p>
+      <p>Innovative.  Technology.  Leader.</p>
+  </body>      
+<html>
